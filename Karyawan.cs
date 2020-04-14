f0bf1e6ec6a4cf664f61e736136fa1b6d41dace4
Asripramudiya <asri.pr@students.amikom.ac.id>
2020-04-14 11:56:36 +0700

    inisialisasi project

---
+++ Karyawan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ResponsiPemrograman2671
+{
+    class Karyawan
+    {
+        public string No { get; set; }
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public void Data()
+        {
+            Console.WriteLine("{0}  {1}{2}  {3}", No, Nik, Nama, GajiBulanan);
+        }
+        public void NaikGaji()
+        {
+            Console.WriteLine("{0}  {1}{2}  {3}", No, Nik, Nama, GajiBulanan+(GajiBulanan*0.1));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ResponsiPemrograman2671
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan();
+            Karyawan karyawan2 = new Karyawan();
+
+            karyawan.No = "1.";
+            karyawan.Nik = 190302123;
+            karyawan.Nama = "Paijo";
+            karyawan.GajiBulanan = 3000000;
+
+            karyawan2.No = "2.";
+            karyawan2.Nik = 190302124;
+            karyawan2.Nama = "Jono";
+            karyawan2.GajiBulanan = 2000000;
+
+            Console.WriteLine("No   NIK/Nama            Gaji Bulanan");
+            Console.WriteLine("--------------------------------------");
+            karyawan.Data();
+            karyawan2.Data();
+
+            Console.WriteLine("Kenaikan Gaji 10%");
+            Console.WriteLine("No   NIK/Nama            Gaji Bulanan");
+            Console.WriteLine("--------------------------------------");
+            karyawan.NaikGaji();
+            karyawan2.NaikGaji();
+
+            Console.ReadKey();
+
+        }
+    }
+}
